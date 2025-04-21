93719c2d936bfbbd47cc0077fc9979e1ade8df65
novaldypratama <novaldypratama@mail.ugm.ac.id>
2025-04-22 00:01:45 +0700

    fix(hardhat): update the localhost URL port to connect to the EthSigner endpoint

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -6,12 +6,12 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:21001",
+      url: "http://127.0.0.1:18545",
       chainID: 1337,
       gasPrice: 0,
       // Replace 'YOUR_PRIVATE_KEY' with a valid private key string (without 0x) for local testing,
       // or use an environment variable, e.g. process.env.PRIVATE_KEY
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : ["797bbe0373132e8c5483515b68ecbb6d3581b56f0205b653ad2b30a559e83891"]
+      //accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : ["797bbe0373132e8c5483515b68ecbb6d3581b56f0205b653ad2b30a559e83891"]
     }
   }
 };
