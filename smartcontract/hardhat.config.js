f6065ebc0ea6c0f66447a5d730fca982b9bc2598
novaldypratama <novaldypratama@mail.ugm.ac.id>
2025-05-06 20:59:05 +0700

    feat(contract): add SimpleContract as a baseline for running Caliper tools

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -6,12 +6,12 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:18545",
+      url: "http://127.0.0.1:21001",
       chainID: 1337,
       gasPrice: 0,
       // Replace 'YOUR_PRIVATE_KEY' with a valid private key string (without 0x) for local testing,
       // or use an environment variable, e.g. process.env.PRIVATE_KEY
-      //accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : ["797bbe0373132e8c5483515b68ecbb6d3581b56f0205b653ad2b30a559e83891"]
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : ["b37a2494f2330ee4fdf516b38bad42b8e27e35e810abf1baf1fb51ad880872ed"]
     }
   }
 };
